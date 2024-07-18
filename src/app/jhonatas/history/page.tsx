ed04b3654a090df253dfdf995d7c3e5356018710
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-07-18 10:38:52 -0300

    feat: create page history

---
+++ src/app/jhonatas/history/page.tsx
@@ -0,0 +1,11 @@
+export default function History() {
+    return (
+      <div className="flex flex-wrap gap-[5px] justify-start items-stretch w-full">
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+      </div>
+    );
+  }
+  
