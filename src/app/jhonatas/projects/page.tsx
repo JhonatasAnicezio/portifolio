63a0efd4300e4891c5e50b4042765e1abf46f962
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-07-18 10:38:15 -0300

    feat: create page projects

---
+++ src/app/jhonatas/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+    return (
+      <div className="flex flex-wrap gap-[5px] justify-start items-stretch w-full">
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+          <div className="w-[307.67px] h-[307.67px] bg-[#262626]" />
+      </div>
+    );
+  }
+  
