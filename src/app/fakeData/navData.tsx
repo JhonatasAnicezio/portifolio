94696b7763d55f7f21240dfd52ed7460a1493aa7
JhonatasAnicezio <jhonatasanic@hotmail.com>
2024-07-18 13:06:21 -0300

    feat: create fakeData

---
+++ src/app/fakeData/navData.tsx
@@ -0,0 +1,21 @@
+import { LiaTableSolid } from "react-icons/lia";
+import { LuHistory } from "react-icons/lu";
+import { MdOutlinePersonPin } from "react-icons/md";
+
+export const pages = [
+  {
+      href: '',
+      name: 'ABOUT',
+      icon: <MdOutlinePersonPin />,
+  },
+  {
+      href: '/projects',
+      name: 'PROJETOS',
+      icon: <LiaTableSolid />,
+  },
+  {
+      href: '/history',
+      name: 'HISTORIA',
+      icon: <LuHistory />,
+  },
+]
