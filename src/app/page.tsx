d965b89cdb19c036108ad6b7527bd729307b97cf
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-03-18 21:35:04 -0300

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         🎯 Atualmente, estou engajado em um projeto voluntário chamado TableRise. O objetivo? Ajudar jogadores de RPG a deixarem suas campanhas mais dinâmicas e divertidas, oferecendo ferramentas intuitivas e práticas.
       </p>
       <p>
-        No meu último trabalho, atuei com infraestrutura de redes 🏗️, adquirindo uma compreensão aprofundada das necessidades tecnológicas de uma empresa. Atualmente, estou cursando Engenharia de Software 🎓, ampliando ainda mais meus conhecimentos na área
+        No meu último trabalho, atuei com infraestrutura de redes 🏗️, adquirindo uma compreensão aprofundada das necessidades tecnológicas de uma empresa. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas 🎓, ampliando ainda mais meus conhecimentos na área
       </p>
       <p>
         🎨 Sou conhecido pela minha criatividade e por estar sempre aberto a novos projetos. Encaro cada desafio como uma oportunidade de crescer e aprender. Para mim, a programação é uma ferramenta poderosa para transformar ideias em realidade e adoro compartilhar esse entusiasmo com outros.
